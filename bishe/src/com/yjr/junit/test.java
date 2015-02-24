fd834b5549614959924d174649ce0841517186c1
495187186 <495187186@qq.com>
2015-02-24 20:59:32 +0800

    修改排版

--- bishe/src/com/yjr/junit/test.java
+++ bishe/src/com/yjr/junit/test.java
@@ -10,24 +10,24 @@ import com.yjr.db.ConnectDatabase;
 
 public class test {
 
-@Test
+	@Test
 	public void testDB() throws Exception{
-System.out.println("dssss");
-	Connection conn = ConnectDatabase.getConn();
-	PreparedStatement pstm = null;
-	ResultSet res = null;
-	String sql = "select * from bishe.user_info";
-	pstm = (PreparedStatement) conn.prepareStatement(sql);
-	res = pstm.executeQuery();
-	while(res.next()){
-		String user = res.getString("user");
-		String password = res.getString("password");
-		System.out.println(user);
-		System.out.println(password);
-	}
-	if(pstm!=null){pstm.close();}
-	if(res!=null){res.close();}
-	if(conn!=null){conn.close();}
+		System.out.println("dssss");
+		Connection conn = ConnectDatabase.getConn();
+		PreparedStatement pstm = null;
+		ResultSet res = null;
+		String sql = "select * from bishe.user_info";
+		pstm = (PreparedStatement) conn.prepareStatement(sql);
+		res = pstm.executeQuery();
+		while(res.next()){
+			String user = res.getString("user");
+			String password = res.getString("password");
+			System.out.println(user);
+			System.out.println(password);
+		}
+		if(pstm!=null){pstm.close();}
+		if(res!=null){res.close();}
+		if(conn!=null){conn.close();}
 }
 
 
